12cd9f5e5cd775d23262f0baa607baa6c02ea617
Antonio Tapiador del Dujo <atapiador@dit.upm.es>
2012-01-25 16:44:31 +0100

    Remove deprecation warnings with activesupport 3.2

--- lib/scheduler/model.rb
+++ lib/scheduler/model.rb
@@ -24,92 +24,90 @@ module Scheduler
       }
     end
 
-    module InstanceMethods
-      def frequency_sym
-        Scheduler::FREQUENCIES[frequency]
+    def frequency_sym
+      Scheduler::FREQUENCIES[frequency]
+    end
+
+    # Get [ 0, 1, 6 ] (monday, tuesday, sunday) from byte
+    def week_days
+      7.times.map{ |i|
+        days[i] > 0 ?
+        i :
+        nil
+      }.compact
+    end
+
+    def week_day
+      week_days.first
+    end
+
+    def week_days_sym
+      week_days.map{ |d|
+        Date::DAYS_INTO_WEEK.invert[d]
+      }
+    end
+
+    def week_day_sym
+      week_days_sym.first
+    end
+
+    # Convert [ 0, 1, 6 ] (monday, tuesday, sunday) to byte
+    def week_days= ds
+      self.days =
+        ds.map{ |d| 2 ** d.to_i }.inject(0, &:+)
+    end
+
+    def week_day= day
+      self.week_days = Array.wrap(day)
+    end
+
+    def week_day_order
+      interval_flag
+    end
+
+    def week_day_order_sym
+      index = interval_flag
+      index -= 1 if index > 0
+
+      Scheduler::ORDER[index]
+    end
+
+    def week_day_order= order
+      self.interval_flag = order
+    end
+
+    # Array including all the dates this Scheduler has ocurrences
+    # between st and en
+    def ocurrences(st, en = nil)
+      recurrence ?
+        recurrence.events(:starts => st, :until => en) :
+        (start_at.to_date..end_at.to_date).to_a
+    end
+
+    def recurrence
+      case frequency_sym
+      when :weekly
+        Recurrence.new({ :every    => :week,
+                         :starts   => start_date.to_date,
+                         :until    => end_date.try(:to_date),
+                         :interval => interval,
+                         :on       => week_days_sym })
+      when :monthly
+        Recurrence.new({ :every    => :month,
+                         :starts   => start_date.to_date,
+                         :until    => end_date.try(:to_date),
+                         :interval => interval,
+                         :on       => week_day_order_sym,
+                         :weekday => week_day_sym })
       end
+    end
 
-      # Get [ 0, 1, 6 ] (monday, tuesday, sunday) from byte
-      def week_days
-        7.times.map{ |i|
-          days[i] > 0 ?
-          i :
-          nil
-        }.compact
-      end
+    protected
 
-      def week_day
-        week_days.first
-      end
+    def fill_scheduler_dates
+      self.start_date ||= start_at
 
-      def week_days_sym
-        week_days.map{ |d|
-          Date::DAYS_INTO_WEEK.invert[d]
-        }
-      end
-
-      def week_day_sym
-        week_days_sym.first
-      end
-
-      # Convert [ 0, 1, 6 ] (monday, tuesday, sunday) to byte
-      def week_days= ds
-        self.days =
-          ds.map{ |d| 2 ** d.to_i }.inject(0, &:+)
-      end
-
-      def week_day= day
-        self.week_days = Array.wrap(day)
-      end
-
-      def week_day_order
-        interval_flag
-      end
-
-      def week_day_order_sym
-        index = interval_flag
-        index -= 1 if index > 0
-
-        Scheduler::ORDER[index]
-      end
-
-      def week_day_order= order
-        self.interval_flag = order
-      end
-
-      # Array including all the dates this Scheduler has ocurrences
-      # between st and en
-      def ocurrences(st, en = nil)
-        recurrence ?
-          recurrence.events(:starts => st, :until => en) :
-          (start_at.to_date..end_at.to_date).to_a
-      end
-
-      def recurrence
-        case frequency_sym
-        when :weekly
-          Recurrence.new({ :every    => :week,
-                           :starts   => start_date.to_date,
-                           :until    => end_date.try(:to_date),
-                           :interval => interval,
-                           :on       => week_days_sym })
-        when :monthly
-          Recurrence.new({ :every    => :month,
-                           :starts   => start_date.to_date,
-                           :until    => end_date.try(:to_date),
-                           :interval => interval,
-                           :on       => week_day_order_sym,
-                           :weekday => week_day_sym })
-        end
-      end
-
-      protected
-
-      def fill_scheduler_dates
-        self.start_date ||= start_at
-
-        self.end_date = end_at if frequency == 0
-      end
+      self.end_date = end_at if frequency == 0
     end
   end
 end
