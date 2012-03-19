037f066c20e7b838adcda86e73bfc7ef68db3a0b
Antonio Tapiador del Dujo <atapiador@dit.upm.es>
2012-03-19 13:49:19 +0100

    Frequency descriptions

--- lib/scheduler/model.rb
+++ lib/scheduler/model.rb
@@ -28,6 +28,24 @@ module Scheduler
       Scheduler::FREQUENCIES[frequency]
     end
 
+    def frequency_description
+      return "" if frequency_sym == :one_off
+
+      options = { :count => interval }
+
+      case frequency_sym
+      when :weekly
+        options[:week_days] = week_days_in_words
+      when :monthly
+        options[:week_day] = week_days_in_words
+        options[:week_day_order] =
+          I18n.t("scheduler.frequency.monthly.description.week_day_order.#{ week_day_order }")
+      end
+
+      I18n.t "scheduler.frequency.#{ frequency_sym }.description", options
+
+    end
+
     # Get [ 0, 1, 6 ] (monday, tuesday, sunday) from byte
     def week_days
       7.times.map{ |i|
@@ -35,6 +53,12 @@ module Scheduler
         i :
         nil
       }.compact
+    end
+
+    def week_days_in_words
+      week_days.map { |d|
+        I18n.l Date.today.beginning_of_week + d, :format => "%A"
+      }.to_sentence
     end
 
     def week_day
